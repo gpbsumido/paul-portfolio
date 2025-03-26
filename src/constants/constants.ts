b9e9919d0aa32e60ba250fbecaed32c2863d7499
gpbsumido <gpbsumido@gmail.com>
2025-03-25 22:56:06 -0400

    add ua project

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -7,6 +7,16 @@ import portal1 from '../assets/helika_portal.png';
 import portal2 from '../assets/helika_portal_2.png';
 import portal3 from '../assets/helika_portal_3.png';
 import portal4 from '../assets/helika_portal_4.png';
+import portal5 from '../assets/helika_portal_5.png';
+import portal6 from '../assets/helika_portal_6.png';
+
+import ua from '../assets/ua_helika.png';
+import ua1 from '../assets/ua_helika_1.png';
+import ua2 from '../assets/ua_helika_2.png';
+import ua3 from '../assets/ua_helika_3.png';
+import ua4 from '../assets/ua_helika_4.png';
+import ua5 from '../assets/ua_helika_5.png';
+
 
 export const figmaDesigns = [
     "https://embed.figma.com/design/CSlimnvD8zLPOzzF7d97Hu/Sunbow?node-id=0-1&embed-host=share",
@@ -26,4 +36,15 @@ export const HELIKA_PORTAL_IMAGES = [
     portal2,
     portal3,
     portal4,
+    portal5,
+    portal6,
+]
+
+export const HELIKA_UA_IMAGES = [
+    ua,
+    ua1,
+    ua2,
+    ua3,
+    ua4,
+    ua5,
 ]
